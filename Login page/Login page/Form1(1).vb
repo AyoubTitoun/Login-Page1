dbfc6131588933f567ac53238174d758642079a9
Ayoub <ia_titoun@esi.dz>
2020-04-07 16:17:04 +0100

    Add files via upload

---
+++ Login page/Login page/Form1(1).vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+       
+
+    End Sub
+    Private Sub Form1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle,color.MediumBlue,ButtonBorderStyle.Solid)
+    End Sub
+
+    
+
+    Private Sub BunifuCheckbox1_OnChange(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BunifuCheckbox1.OnChange
+
+
+
+
+        Me.Refresh()
+    End Sub
+
+    Private Sub Label5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
---
+++ Login page/Login page/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.BunifuFlatButton1 = New Bunifu.Framework.UI.BunifuFlatButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.BunifuCheckbox1 = New Bunifu.Framework.UI.BunifuCheckbox()
+        Me.BunifuMaterialTextbox3 = New Bunifu.Framework.UI.BunifuMaterialTextbox()
+        Me.BunifuMaterialTextbox2 = New Bunifu.Framework.UI.BunifuMaterialTextbox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Red
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(628, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(15, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "X"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Red
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(289, 33)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 24)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Signup"
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.White
+        Me.Panel3.Controls.Add(Me.LinkLabel1)
+        Me.Panel3.Controls.Add(Me.BunifuFlatButton1)
+        Me.Panel3.Controls.Add(Me.Label5)
+        Me.Panel3.Controls.Add(Me.BunifuCheckbox1)
+        Me.Panel3.Controls.Add(Me.BunifuMaterialTextbox3)
+        Me.Panel3.Controls.Add(Me.BunifuMaterialTextbox2)
+        Me.Panel3.Controls.Add(Me.PictureBox1)
+        Me.Panel3.Location = New System.Drawing.Point(160, 77)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(324, 282)
+        Me.Panel3.TabIndex = 2
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.ActiveLinkColor = System.Drawing.Color.DeepPink
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.MediumBlue
+        Me.LinkLabel1.Location = New System.Drawing.Point(175, 242)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(116, 16)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Forget password?"
+        '
+        'BunifuFlatButton1
+        '
+        Me.BunifuFlatButton1.Activecolor = System.Drawing.Color.Navy
+        Me.BunifuFlatButton1.BackColor = System.Drawing.Color.MediumBlue
+        Me.BunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BunifuFlatButton1.BorderRadius = 0
+        Me.BunifuFlatButton1.ButtonText = "Login"
+        Me.BunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BunifuFlatButton1.DisabledColor = System.Drawing.Color.Navy
+        Me.BunifuFlatButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent
+        Me.BunifuFlatButton1.Iconimage = Nothing
+        Me.BunifuFlatButton1.Iconimage_right = Nothing
+        Me.BunifuFlatButton1.Iconimage_right_Selected = Nothing
+        Me.BunifuFlatButton1.Iconimage_Selected = Nothing
+        Me.BunifuFlatButton1.IconMarginLeft = 0
+        Me.BunifuFlatButton1.IconMarginRight = 0
+        Me.BunifuFlatButton1.IconRightVisible = True
+        Me.BunifuFlatButton1.IconRightZoom = 0.0R
+        Me.BunifuFlatButton1.IconVisible = True
+        Me.BunifuFlatButton1.IconZoom = 90.0R
+        Me.BunifuFlatButton1.IsTab = False
+        Me.BunifuFlatButton1.Location = New System.Drawing.Point(28, 209)
+        Me.BunifuFlatButton1.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.BunifuFlatButton1.Name = "BunifuFlatButton1"
+        Me.BunifuFlatButton1.Normalcolor = System.Drawing.Color.MediumBlue
+        Me.BunifuFlatButton1.OnHovercolor = System.Drawing.Color.Blue
+        Me.BunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White
+        Me.BunifuFlatButton1.selected = False
+        Me.BunifuFlatButton1.Size = New System.Drawing.Size(263, 30)
+        Me.BunifuFlatButton1.TabIndex = 5
+        Me.BunifuFlatButton1.Text = "Login"
+        Me.BunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.BunifuFlatButton1.Textcolor = System.Drawing.Color.White
+        Me.BunifuFlatButton1.TextFont = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DimGray
+        Me.Label5.Location = New System.Drawing.Point(54, 183)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(117, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Show Password"
+        '
+        'BunifuCheckbox1
+        '
+        Me.BunifuCheckbox1.BackColor = System.Drawing.Color.White
+        Me.BunifuCheckbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.BunifuCheckbox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.BunifuCheckbox1.ChechedOffColor = System.Drawing.Color.White
+        Me.BunifuCheckbox1.Checked = False
+        Me.BunifuCheckbox1.CheckedOnColor = System.Drawing.Color.MediumBlue
+        Me.BunifuCheckbox1.ForeColor = System.Drawing.Color.White
+        Me.BunifuCheckbox1.Location = New System.Drawing.Point(28, 183)
+        Me.BunifuCheckbox1.Name = "BunifuCheckbox1"
+        Me.BunifuCheckbox1.Size = New System.Drawing.Size(20, 20)
+        Me.BunifuCheckbox1.TabIndex = 3
+        '
+        'BunifuMaterialTextbox3
+        '
+        Me.BunifuMaterialTextbox3.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.BunifuMaterialTextbox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.BunifuMaterialTextbox3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.BunifuMaterialTextbox3.HintForeColor = System.Drawing.Color.Empty
+        Me.BunifuMaterialTextbox3.HintText = ""
+        Me.BunifuMaterialTextbox3.isPassword = True
+        Me.BunifuMaterialTextbox3.LineFocusedColor = System.Drawing.Color.Blue
+        Me.BunifuMaterialTextbox3.LineIdleColor = System.Drawing.Color.MediumBlue
+        Me.BunifuMaterialTextbox3.LineMouseHoverColor = System.Drawing.Color.Blue
+        Me.BunifuMaterialTextbox3.LineThickness = 3
+        Me.BunifuMaterialTextbox3.Location = New System.Drawing.Point(28, 126)
+        Me.BunifuMaterialTextbox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.BunifuMaterialTextbox3.Name = "BunifuMaterialTextbox3"
+        Me.BunifuMaterialTextbox3.Size = New System.Drawing.Size(263, 33)
+        Me.BunifuMaterialTextbox3.TabIndex = 2
+        Me.BunifuMaterialTextbox3.Text = "Password"
+        Me.BunifuMaterialTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'BunifuMaterialTextbox2
+        '
+        Me.BunifuMaterialTextbox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.BunifuMaterialTextbox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.BunifuMaterialTextbox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.BunifuMaterialTextbox2.HintForeColor = System.Drawing.Color.Empty
+        Me.BunifuMaterialTextbox2.HintText = ""
+        Me.BunifuMaterialTextbox2.isPassword = False
+        Me.BunifuMaterialTextbox2.LineFocusedColor = System.Drawing.Color.Blue
+        Me.BunifuMaterialTextbox2.LineIdleColor = System.Drawing.Color.MediumBlue
+        Me.BunifuMaterialTextbox2.LineMouseHoverColor = System.Drawing.Color.Blue
+        Me.BunifuMaterialTextbox2.LineThickness = 3
+        Me.BunifuMaterialTextbox2.Location = New System.Drawing.Point(28, 72)
+        Me.BunifuMaterialTextbox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.BunifuMaterialTextbox2.Name = "BunifuMaterialTextbox2"
+        Me.BunifuMaterialTextbox2.Size = New System.Drawing.Size(263, 33)
+        Me.BunifuMaterialTextbox2.TabIndex = 1
+        Me.BunifuMaterialTextbox2.Text = "UserName"
+        Me.BunifuMaterialTextbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.login
+        Me.PictureBox1.Location = New System.Drawing.Point(117, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(655, 371)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents BunifuMaterialTextbox1 As Bunifu.Framework.UI.BunifuMaterialTextbox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents BunifuMaterialTextbox3 As Bunifu.Framework.UI.BunifuMaterialTextbox
+    Friend WithEvents BunifuMaterialTextbox2 As Bunifu.Framework.UI.BunifuMaterialTextbox
+    Friend WithEvents BunifuCheckbox1 As Bunifu.Framework.UI.BunifuCheckbox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents BunifuFlatButton1 As Bunifu.Framework.UI.BunifuFlatButton
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
